c1517673a4a05c7cc6a679561a24c5920d25fba1
selmanyberg <selma@selmis.mynet>
2023-06-01 18:13:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dictionaryapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dictionaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
